84df41b8c8d13838489dd675c3ea9fbe0967050b
njaup <njaup974@gmail.com>
2023-07-26 08:38:03 -0700

    shell.h

--- setenv_unsetenv.c
+++ setenv_unsetenv.c
@@ -6,101 +6,96 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * write_to_stderr - writes the specified message to the standard error stream
- * @message: written to stderr
- */
 void write_to_stderr(const char *message)
 {
-	write(STDERR_FILENO, message, strlen(message));
+    write(STDERR_FILENO, message, strlen(message));
 }
-/**
- * execute_command - executes other shell commands by forking a new process
- * @args: An array of strings 
- */
-void execute_command(char *args[])
-{
-	pid_t pid = fork();
 
-	if (pid < 0)
+void handle_setenv(char *args[])
+{
+	if (args[1] && args[2])
 	{
-		perror("fork");
-		return;
-	}
-	else if (pid == 0)
-	{
-		execvp(args[0], args);
-		perror("execvp");
-		_exit(EXIT_FAILURE);
+		int overwrite = 1;
+		
+		if (setenv(args[1], args[2], overwrite) != 0)
+		{
+			write_to_stderr("setenv: Failed to set environment variable\n");
+		}
 	}
 	else
 	{
-		wait(NULL);
+		write_to_stderr("setenv: Missing variable or value\n");
 	}
 }
-/**
- * process_command - processes the user input command by parsing & executing
- * @command: A string containing the user's command input
- */
+
+void handle_unsetenv(char *args[])
+{
+	if (args[1])
+	{
+		if (unsetenv(args[1]) != 0)
+		{
+			write_to_stderr("unsetenv: Failed to unset environment variable\n");
+		}
+	}
+	else
+	{
+		write_to_stderr("unsetenv: Missing variable\n");
+	}
+}
+
 void process_command(char *command)
 {
 	char *args[4] = { NULL }, *token = strtok(command, " \n");
-	int i = 0, overwrite = 1;
-
+	int i = 0;
+	
 	while (token != NULL && i < 3)
 	{
-		args[i++] = token;
-		token = strtok(NULL, " \n");
+		args[i++] = token, token = strtok(NULL, " \n");
 	}
 	args[i] = NULL;
+	
 	if (i == 0)
 	{
 		return;
 	}
+	
 	if (strcmp(args[0], "setenv") == 0)
 	{
-		if (args[1] && args[2])
-		{
-			if (setenv(args[1], args[2], overwrite) != 0)
-			{
-				write_to_stderr("setenv: failed to set environment variable\n");
-			}
-		}
-		else
-		{
-			write_to_stderr("setenv: missing variable or value\n");
-		}
+		handle_setenv(args);
 	}
 	else if (strcmp(args[0], "unsetenv") == 0)
 	{
-		if (args[1])
-		{
-			if (unsetenv(args[1]) != 0)
-			{
-				write_to_stderr("unsetenv: failed to unset environment variable\n");
-			}
-		}
-		else
-		{
-			write_to_stderr("unsetenv: missing variable\n");
-		}
+		handle_unsetenv(args);
 	}
 	else
 	{
-		execute_command(args);
+		pid_t pid = fork();
+		
+		if (pid < 0)
+		{
+			perror("fork");
+			return;
+		}
+		else if (pid == 0)
+		{
+			execvp(args[0], args);
+			perror("execvp");
+			_exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(NULL);
+		}
 	}
 }
-/**
- * main -  implements the simple shell
- * 
- */
+
 int main(void)
 {
 	while (1)
 	{
-		write_to_stderr("your shell prompt $ ");
+		write_to_stderr("Your shell prompt $ ");
 		char command[256];
-
+		
 		if (fgets(command, sizeof(command), stdin) == NULL)
 		{
 			break;
--- shell.h
+++ shell.h
@@ -4,7 +4,6 @@
 #define MAX_LENGTH 1024
 #define MAX_ARGS 64
 
-#define MAX_COMMAND_LENGTH 100
 #define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGUMENTS 64
 
@@ -19,9 +18,9 @@ struct Alias
     char value[MAX_ALIAS_VALUE];
 };
 
-void print_aliases(const struct * aliases, int num_aliases);
-void set_alias(struct * aliases, int *num_aliases, const char *name, const char *value);
-void parse_alias_command(const char *command, struct * aliases, int *num_aliases);
+void print_aliases(const struct Alias* aliases, int num_aliases);
+void set_alias(struct Alias* aliases, int *num_aliases, const char *name, const char *value);
+void parse_alias_command(const char *command, struct Alias* aliases, int *num_aliases);
 void write_string(const char *str, int out_fd);
 void execute_command(char *command);
 int main(void);
